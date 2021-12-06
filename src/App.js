e67f55d3abc3f4df19d68e5801941a8c2e95126f
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2021-12-06 15:20:01 +0300

    Navbar.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,52 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
+
+import {Home,SingleProduct,Cart,Checkout,Error,About,Products,Private} from './pages'
+
+
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return <Router>
+    <Navbar/>
+    <Sidebar/>
+    <Footer/>
+    
+
+    <Switch>
+
+      <Route exact path="/">
+        <Home/>
+      </Route>
+
+      <Route exact path="/about">
+        <About/>
+      </Route>
+
+      <Route exact path="/cart">
+        <Cart/>
+      </Route>
+
+      <Route exact path="/products">
+        <Products/>
+      </Route>
+
+      <Route exact path="/products/:id" children={<SingleProduct/>}>
+
+      </Route>
+
+      <Route exact path="/checkout">
+        <Checkout/>
+      </Route>
+
+      <Route  path="*">
+        <Error/>
+      </Route>
+    
+
+
+
+    </Switch>
+  </Router>
 }
 
 export default App
